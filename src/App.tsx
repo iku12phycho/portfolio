2fafbbf7479686b1d8cf7315eb3b273b10e36f51
Kensuke Ikunaga <iku12phycho@gmail.com>
2021-11-05 06:46:20 +0900

    changed home path

--- src/App.tsx
+++ src/App.tsx
@@ -13,15 +13,15 @@ function App() {
       <Nav />
       <Box className='App-header'>
           <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/Profile">
+            <Route exact path="/portfolio/Profile">
               <Profile />
             </Route>
           </Switch>
-            <Route exact path="/Skill">
+            <Route exact path="/portfolio/Skill">
               <Skill />
+            </Route>
+            <Route path="/">
+              <Home />
             </Route>
       </Box>
     </div>
